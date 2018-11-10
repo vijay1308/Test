af07db62476217798c7be03a57730d23552d8c6d
Vaibhav Joshi <vaibhavjoshi@cdnsol.com>
2018-11-10 18:44:39 +0530

    Dhanfge

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,22 @@ class Project < ActiveRecord::Base
   attr_accessible  :description, :name, :user_id, :contributors_attributes
   has_many :contributors
   has_many :users, :through => :contributors
+  has_many :tasks
 
   accepts_nested_attributes_for :contributors, :allow_destroy => true
+
+  def get_chart_data 
+    data = []
+    todo = ['todo status', 'Todo status count']
+    data << todo
+    todos = self.tasks.group_by {|a| a.status_type_id}
+    todos.each do |status, records|
+      todo_data = []
+      todo_data << status.to_s
+      todo_data << records.count
+      data << todo_data
+    end
+    data
+  end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-  attr_accessible  :asigned_use_id, :description,  :name
+  attr_accessible  :asigned_use_id, :description,  :name, :project_id, :status_type_id
 end
